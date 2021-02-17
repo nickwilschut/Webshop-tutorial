ebb1020d1e6e4709a4f8aa9f29efddd755e7b104
nickwilschut <nickwilschut@gmail.com>
2021-02-17 16:24:45 +0100

    updated new favicon

--- Webshop/resources/views/layouts/app.blade.php
+++ Webshop/resources/views/layouts/app.blade.php
@@ -18,6 +18,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link rel="icon" href="{{ URL::asset('favicon.ico') }}" type="image/x-icon"/>
 </head>
 <body>
     <div id="app">
